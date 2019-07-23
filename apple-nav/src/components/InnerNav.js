8decc82e7cf782da2cb5c0711b8ce801417864cf
Lou <antilou86@gmail.com>
2019-07-23 19:10:13 -0400

    WIP: began adjusting code to allow my component to access customer URL string in a useEffect call, ran out of time.

--- apple-nav/src/components/InnerNav.js
+++ apple-nav/src/components/InnerNav.js
@@ -6,7 +6,12 @@ const InnerNav = (props) => {
     const {topdata} = props;
     //have it loop through topdata and compare each topdata.name against the :/itemName prop
     //then if it matches, render the below cards using the sublink data within the named item.
-    const itemNames = props.match.params.itemName;
+    
+    useEffect(() => {
+        const itemNames = props.match.params.itemName;
+      }, [props.match.params.itemName])
+    
+      
     console.log(props)
     console.log(itemNames);
 
